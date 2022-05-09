99c0e96665084385466c85eebb0f020be744d487
sedaliSedalian <seyedalian@outlook.com>
2022-05-09 11:18:40 +0430

    form and database interaction

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from .models import List
-
+from .forms import ListForm
 
 # Create your views here.
 def home(request):
+    if request.method == "POST":  # add form input item to database
+        if request.POST["item"] != "" :
+            form = ListForm(request.POST)
+            form.save()
+    
     all_items = List.objects.all
     return render(request,'home.html',{'all_items': all_items})
 
