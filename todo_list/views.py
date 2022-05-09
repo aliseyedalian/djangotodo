6c72e341874983b7085eeba997d4daa58f85c01a
sedaliSedalian <seyedalian@outlook.com>
2022-05-09 12:01:57 +0430

    Add Messages and alert

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import List
 from .forms import ListForm
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
@@ -8,6 +9,7 @@ def home(request):
         if request.POST["item"] != "" :
             form = ListForm(request.POST)
             form.save()
+            messages.success(request,('Item "{}" Has Been Added To List!'.format(request.POST["item"])))
     
     all_items = List.objects.all
     return render(request,'home.html',{'all_items': all_items})
